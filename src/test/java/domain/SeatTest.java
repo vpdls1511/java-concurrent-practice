9d8bf7d5c98f8a46ba9dc90d808d36e743b495c6
nGyu <vpdls1511@gmail.com>
2026-07-22 19:06:28 +0900

    test : 도메인 요구사항 정의 및 테스트 작성

---
+++ src/test/java/domain/SeatTest.java
@@ -0,0 +1,83 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SeatTest {
+
+  @Test
+  @DisplayName("좌석을 생성할 수 있으며, 최초에는 AVAILABLE 상태다.")
+  void createSeat() {
+    //given
+    String seatNo = "A-01";
+
+    //when
+    Seat seat = Seat.create(seatNo);
+
+    //then
+    assertEquals(seatNo, seat.getNo());
+    assertEquals(SeatStatus.AVAILABLE, seat.getStatus());
+  }
+
+  @Test
+  @DisplayName("사용자는 좌석을 선점하면 결제 대기 상태가 된다.")
+  void reserveSeatThenWaitingPayment() {
+    //given
+    String seatNo = "A-01";
+    Seat seat = Seat.create(seatNo);
+
+    //when
+    seat.reserve();
+
+    //then
+    assertEquals(SeatStatus.PAYMENT_WAIT, seat.getStatus());
+  }
+
+  @Test
+  @DisplayName("결제 대기중인 좌석은 구매할 수 없다.")
+  void alreadySeatProcess() {
+    //given
+    String seatNo = "A-01";
+    Seat seat = Seat.create(seatNo);
+
+    //when
+    seat.reserve();
+
+    // then
+    assertThrows(IllegalStateException.class, seat::reserve);
+  }
+
+  @Test
+  @DisplayName("결제가 완료되면 좌석은 예약 상태가 된다.")
+  void reserveSeat() {
+    //given
+    String seatNo = "A-01";
+    Seat seat = Seat.create(seatNo);
+
+    //when
+    seat.reserve();
+    seat.payment();
+
+    //then
+    assertEquals(SeatStatus.RESERVATION, seat.getStatus());
+  }
+
+  @Test
+  @DisplayName("이미 선정된 좌석은 예약할 수 없다.")
+  void alreadySeat() {
+    //given
+    String seatNo = "A-01";
+    Seat seat = Seat.create(seatNo);
+
+    //when
+    seat.reserve();
+    seat.payment();
+
+    //then
+    assertThrows(IllegalStateException.class, seat::reserve);
+    assertThrows(IllegalStateException.class, seat::payment);
+  }
+}
