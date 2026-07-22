e645b01f18f6f0406f69664592cece6d7235786e
nGyu <vpdls1511@gmail.com>
2026-07-22 19:09:27 +0900

    feat : 도메인 로직 작성

---
+++ src/main/java/domain/Seat.java
@@ -0,0 +1,44 @@
+package domain;
+
+public class Seat {
+
+  private final String no;
+  private SeatStatus status;
+
+  public Seat(String no) {
+    this.no = no;
+    this.status = SeatStatus.AVAILABLE;
+  }
+
+  /**
+   * 좌석을 생성할 수 있다.
+   *
+   * @param no
+   * @return
+   */
+  public static Seat create(String no) {
+    return new Seat(no);
+  }
+
+  public String getNo() {
+    return this.no;
+  }
+
+  public SeatStatus getStatus() {
+    return this.status;
+  }
+
+  public void reserve() {
+    if (!SeatStatus.AVAILABLE.equals(this.status)) {
+      throw new IllegalStateException("이미 선점된 좌석 입니다.");
+    }
+    this.status = SeatStatus.PAYMENT_WAIT;
+  }
+
+  public void payment() {
+    if (!SeatStatus.PAYMENT_WAIT.equals(this.status)) {
+      throw new IllegalStateException("선점한 좌석이 아닙니다.");
+    }
+    this.status = SeatStatus.RESERVATION;
+  }
+}
---
+++ src/main/java/domain/SeatStatus.java
@@ -0,0 +1,7 @@
+package domain;
+
+public enum SeatStatus {
+  AVAILABLE, // 대기
+  PAYMENT_WAIT, // 결제 대기중
+  RESERVATION // 예약완료
+}
